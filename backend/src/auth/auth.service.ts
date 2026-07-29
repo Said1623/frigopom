c591306e82365e5786802be66cb3169e71f3ec53
Said <aboulouafa1957@gmail.com>
2026-07-29 19:59:48 +0100

    fix: replace bcrypt with bcryptjs

--- backend/src/auth/auth.service.ts
+++ backend/src/auth/auth.service.ts
@@ -2,7 +2,7 @@ import { Injectable, UnauthorizedException, ConflictException } from '@nestjs/co
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { JwtService } from '@nestjs/jwt';
-import * as bcrypt from 'bcrypt';
+import * as bcrypt from 'bcryptjs';
 import { User } from './user.entity';
 
 @Injectable()
